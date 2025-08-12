d827972d629320f7ce9a904f62f2982b24235df7
Rabinagurung <rabinagurung347@gmail.com>
2025-08-12 18:47:45 -0400

    Convex Package Setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,18 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client"
+
+import { useMutation, useQuery} from "convex/react";
+import {api} from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+
+  const users = useQuery(api.users.getMany);
+  const onAddClick = useMutation(api.users.add)
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello App Web</h1>
-        <p>{add(10, 20)}</p>
-        <Input/>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+    <p>Web app </p>
+    <Button onClick={()=>onAddClick()}>Add</Button>
+    <div className="max-w-sm w-full mx-auto">{JSON.stringify(users, null, 2)}</div>
     </div>
   )
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,23 @@
-import { add } from "@workspace/math/add"
+"use client"
+
+import { api } from "@workspace/backend/_generated/api"
 import { Button } from "@workspace/ui/components/button"
+import { useMutation, useQuery } from "convex/react"
+
+
 
 export default function Page() {
+
+  const users = useQuery(api.users.getMany)
+  const onAddClick = useMutation(api.users.add)
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello App/Wdiget</h1>
-        <p>{add(2,5)}</p>
-        <Button size="sm">Button</Button>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p>Hello App/Wdiget</p>
+        <Button onClick={() => onAddClick()}>Add</Button>
+        <div className="max-w-sm">
+          {JSON.stringify(users, null, 2)}
+        </div>
       </div>
-    </div>
   )
 }
