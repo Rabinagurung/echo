e8ecbde201f8f61e69c76f4fc1e4d6241df8cc22
Rabinagurung <rabinagurung347@gmail.com>
2026-03-28 20:00:10 -0700

    fix widget domain

--- apps/web/public/embed.js
+++ apps/web/public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const o = {
-      WIDGET_URL: "echo-widget-vert.vercel.app",
+      WIDGET_URL: "https://echo-widget-vert.vercel.app",
       DEFAULT_POSITION: "bottom-right",
     },
     u = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
